b3c43fb19d06115eaee8fc14960f393868b964f8
Van Hamme Antoine <antoine.v.hamme@gmail.com>
2019-03-28 11:57:49 +0100

    added hide event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HighlightDirective } from './highlight.directive';
+import { DisplayMoviesDirective } from './display-movies.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
     UserProfileComponent,
-    HighlightDirective
+    HighlightDirective,
+    DisplayMoviesDirective
   ],
   imports: [
     BrowserModule,
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -17,18 +17,7 @@ export class UserProfileComponent implements OnInit {
     photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
   };
 
-  showMovies = {
-    movie1: 'The Godfather',
-    movie2: 'The Shawshank Redemption (1994)',
-    movie3: 'Gone With the Wind (1939)',
-    movie4: 'Casablanca (1942)',
-    movie5: 'It\'s a Wonderful Life (1946)',
-    movie6: 'Schindler\'s List (1993)',
-    movie7: 'Star Wars',
-    movie8: 'Rear Window (1954)',
-    movie9: 'Citizen Kane (1941)',
-    movie10: 'To Kill a Mockingbird (1962)'
-  };
+  showMovies = true
 
   hideButton(event: any) {
     if (this.hidden === true) {
